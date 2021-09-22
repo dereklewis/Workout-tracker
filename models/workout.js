3008feb5ecebadaae2e28f24a8f72d5401248873
Derek Lewis <derek.lewis.den@outlook.com>
2021-09-22 15:56:04 -0600

    cardio schema completed

--- models/workout.js
+++ models/workout.js
@@ -37,3 +37,28 @@ const resistanceSchema = new Schema({
     required: "Number of minutes is required",
   },
 });
+
+const cardioSchema = new Schema({
+  exType: {
+    type: String,
+    required: true,
+  },
+
+  exName: {
+    type: String,
+    trim: true,
+    required: "Exercise Name is required",
+  },
+
+  distance: {
+    type: Number,
+    trim: true,
+    required: "Weight is required",
+  },
+
+  duration: {
+    type: Number,
+    trim: true,
+    required: "Number of minutes is required",
+  },
+});
