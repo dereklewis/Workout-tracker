3c2096d34e016007bd06f8f9bddf3798ec8ee058
Derek Lewis <derek.lewis.den@outlook.com>
2021-09-25 17:07:36 -0600

    post route to /workouts created

--- api/routes.js
+++ api/routes.js
@@ -1,35 +1,31 @@
 const express = require("express");
 const router = express.Router();
-// const Workout = require("../models/workout");
+const Workout = require("../models/workout");
 // const Exercise = require("../public/exercise");
 // const api = require("../public/api");
 
 
 
 router.post("/workouts", async (req, res) => {
-    console.info(req.body)
-})
+    Workout.create({})
+    .then(workout => {
+        res.status(201).json(workout)
+    })
+      .catch(({ message }) => {
+      console.log(message);
+    });
+});
 
-// router.post("/workouts", async ({ body }, res) => {
-//   const workOut = new Workout(body);
-// //   workOut.initExercise();
-//   Workout.create(workOut)
-//     .then((workout) => {
-//       console.log(workout);
+// router.put("/workouts", async (req, res) => {
+//     Workout.create({})
+//     .then(workout => {
+//         res.status(201).json(workout)
 //     })
-//     .catch(({ message }) => {
+//       .catch(({ message }) => {
 //       console.log(message);
 //     });
 // });
 
-// router.post("/workouts", ({ body }, res) => {
-//   Workout.create(body)
-//     .then((Workout) => {
-//       res.json(Workout);
-//     })
-//     .catch((err) => {
-//       res.json(err);
-//     });
-// });
+
 
 module.exports = router;
