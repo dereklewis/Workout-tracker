15dbfbe4b83e1407f52687d7ae96a3a6015fe4f3
Derek Lewis <derek.lewis.den@outlook.com>
2021-09-25 17:51:03 -0600

    changes made to the put route

--- api/routes.js
+++ api/routes.js
@@ -1,30 +1,31 @@
 const express = require("express");
 const router = express.Router();
 const Workout = require("../models/workout");
+
 // const Exercise = require("../public/exercise");
 // const api = require("../public/api");
 
 
 
-router.post("/workouts", async (req, res) => {
-    Workout.create({})
+router.post("/workouts", async ({body}, res) => {
+    Workout.create(body)
     .then(workout => {
-        res.status(201).json(workout)
+        res.json(workout);
     })
       .catch(({ message }) => {
       console.log(message);
     });
 });
 
-// router.put("/workouts", async (req, res) => {
-//     Workout.create({})
-//     .then(workout => {
-//         res.status(201).json(workout)
-//     })
-//       .catch(({ message }) => {
-//       console.log(message);
-//     });
-// });
+router.put("/workouts/:id", async ({body}, res) => {
+    Workout.update(body)
+    .then(workout => {
+        res.json(workout);
+    })
+      .catch(({ message }) => {
+      console.log(message);
+    });
+});
 
 
 
--- server.js
+++ server.js
@@ -17,25 +17,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/trackerdb", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false,
 });
 
-
-
-
-
-
-// Workout.create({ name: "Workout" })
-//   .then((Workout) => {
-//     console.log(Workout);
-//   })
-//   .catch(({ message }) => {
-//     console.log(message);
-//   });
 
 
 
