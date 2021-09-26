c302daafc3e001efbffbba3ecddbb7bc29ad4643
Derek Lewis <derek.lewis.den@outlook.com>
2021-09-26 12:59:00 -0600

    push for instruction assistance

--- api/routes.js
+++ api/routes.js
@@ -2,42 +2,43 @@ const express = require("express");
 const router = express.Router();
 const Workout = require("../models/workout");
 
-// const Exercise = require("../public/exercise");
-// const api = require("../public/api");
-
-
-
-router.post("/workouts", async ({body}, res) => {
-    Workout.create(body)
-    .then(workout => {
-        res.json(workout);
-        
+router.post("/workouts", async ({ body }, res) => {
+  Workout.create(body)
+    .then((workout) => {
+      res.json(workout);
     })
-      .catch(({ message }) => {
+    .catch(({ message }) => {
       console.log(message);
     });
 });
 
-router.put("/workouts/:id", async ({body}, res) => {
-    Workout.updateOne(body)
-    .then(workout => {
-        res.json(workout);
-    })
-      .catch(({ message }) => {
-      console.log(message);
-    });
+router.put("/workouts/:id", async (req, res) => {
+    const excerciseId = req.params.id;
+
+  Workout.findByIdAndUpdate(excerciseId, { excercises: []},
+    
+
+    { $push: { exercises: req.body}},
+    
+    function (err, result) {
+      if (err) {
+        console.log(err);
+      }
+      console.log("RESULT:" + result);
+      res.json("Done");
+    }
+  );
 });
 
 router.get("/workouts", (req, res) => {
   Workout.find({})
     .sort({ date: -1 })
-    .then(workout => {
+    .then((workout) => {
       res.json(workout);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(400).json(err);
     });
 });
-
 
 module.exports = router;
