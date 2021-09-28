794bf8e24605f7c86980615aca9d617a27d1d563
Derek Lewis <derek.lewis.den@outlook.com>
2021-09-28 15:21:02 -0600

    added potential post route

--- api/routes.js
+++ api/routes.js
@@ -2,41 +2,65 @@ const express = require("express");
 const router = express.Router();
 const Workout = require("../models/workout");
 
-
-router.post("/workouts/", async ({body}, res) => {
+router.post("/workouts/", async ({ body }, res) => {
   Workout.create(body)
-    .then(workouts => {
+    .then((workouts) => {
       res.json(workouts);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(400).json(err);
     });
-
 });
+
+// router.post("/workouts/", async ({ body }, res) => {
+//     const newWorkout = new Workout;
+//     Workout.create({
+//       date: req.body,
+//       exercises: {
+//         type: req.body.type,
+//         name: req.body.name,
+//         duration: req.body.duration,
+//         weight: req.body.weight,
+//         reps: req.body.reps,
+//         sets: req.body.sets,
+//         duration: req.body.duration,
+//         distance: req.body.distance,
+//       }
+//     }),
+//     .then((newWorkout) => {
+//       res.json(newWorkout);
+//     })
+//     .catch((err) => {
+//       res.status(400).json(err);
+//     });
+// });
+
+
+
 
 router.put("/workouts/:id", async (req, res) => {
   Workout.findByIdAndUpdate(
     { _id: req.params.id },
-    { $push: { exercises: req.body } }, { new: true }), 
+    { $push: { exercises: req.body } },
+    { new: true }
+  ),
     function (err, result) {
       if (err) {
         res.send(err);
       } else {
         res.json(Workout.findByIdAndUpdate());
       }
-    }
-  ;
+    };
 });
 
 router.get("/workouts", (req, res) => {
-    Workout.find({})
-    .then(workouts => {
-        res.json(workouts);
+  Workout.find({})
+    .then((workouts) => {
+      res.json(workouts);
     })
-    .catch(err => {
-        res.json(err);
+    .catch((err) => {
+      res.json(err);
     });
 });
-
 
 module.exports = router;
